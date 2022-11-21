bf76159eae06fc3adf3bb8bf0a9e949ca7eba84f
Piotr Ulanowski <pioula111@gmail.com>
2022-11-21 18:37:17 +0100

    del friends execute implementation

--- src/main/java/com/piotr1ulanowski/Command/CommandDelFriends.java
+++ src/main/java/com/piotr1ulanowski/Command/CommandDelFriends.java
@@ -8,9 +8,19 @@ import java.util.Optional;
 public class CommandDelFriends implements CommandI {
     private String user1;
     private String user2;
-    @Override
-    Optional<String> execute(HashMap<String, User> users) {
 
+    public CommandDelFriends(String _user1, String _user2) {
+        user1 = _user1;
+        user2 = _user2;
+    }
+
+    // Removing users from friend lists of each user.
+    @Override
+    public Optional<String> execute(HashMap<String, User> users) {
+        users.get(user1).removeFriend(user2);
+        users.get(user2).removeFriend(user1);
+
+        return Optional.empty();
     }
 
     @Override
