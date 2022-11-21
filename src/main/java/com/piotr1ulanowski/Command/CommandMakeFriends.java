245c0aafb5378cf76ce5ebfedd10b38414d2a8b6
Piotr Ulanowski <pioula111@gmail.com>
2022-11-21 18:37:51 +0100

    add friends execute implementation

--- src/main/java/com/piotr1ulanowski/Command/CommandMakeFriends.java
+++ src/main/java/com/piotr1ulanowski/Command/CommandMakeFriends.java
@@ -1,11 +1,25 @@
 package com.piotr1ulanowski.Command;
 
+import com.piotr1ulanowski.User.User;
+
+import java.util.HashMap;
+import java.util.Optional;
+
 public class CommandMakeFriends implements CommandI {
     private String user1;
     private String user2;
-    @Override
-    public void execute() {
+    public CommandMakeFriends(String _user1, String _user2) {
+        user1 = _user1;
+        user2 = _user2;
+    }
 
+    // Removing users from friend lists of each user.
+    @Override
+    public Optional<String> execute(HashMap<String, User> users) {
+        users.get(user1).addFriend(user2);
+        users.get(user2).addFriend(user1);
+
+        return Optional.empty();
     }
 
     @Override
