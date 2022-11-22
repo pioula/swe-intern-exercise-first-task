365ea0e6f1a2f385298375b2bceba3659ef17eb2
Piotr Ulanowski <pioula111@gmail.com>
2022-11-22 11:52:04 +0100

    Added test - testAddingProperty_WhenSmallerTimestamp_ShouldNotUpdateTheProperty

--- src/test/java/com/piotr1ulanowski/UserTests/UserTests.java
+++ src/test/java/com/piotr1ulanowski/UserTests/UserTests.java
@@ -20,7 +20,7 @@ class UserTests {
         final UserProperty FIRST_UPDATE = new UserProperty("test1", 1);
         final UserProperty SECOND_UPDATE = new UserProperty("test2", 2);
 
-        User testUser = new User("test_id");
+        User testUser = new User(USER_ID);
 
         testUser.addProperty("test_property", FIRST_UPDATE);
         testUser.addProperty("test_property", SECOND_UPDATE);
@@ -29,6 +29,25 @@ class UserTests {
                 Objects.equals(testUser.getProperties().get(PROPERTY_NAME).getTimestamp(),
                         SECOND_UPDATE.getTimestamp()) &&
                 testUser.getProperties().get(PROPERTY_NAME).getName().equals(SECOND_UPDATE.getName()));
+    }
+
+    @DisplayName("Should not add a property with a smaller timestamp.")
+    @Test
+    void testAddingProperty_WhenSmallerTimestamp_ShouldNotUpdateTheProperty() {
+        final String USER_ID = "test_id";
+        final String PROPERTY_NAME = "test_property";
+        final UserProperty FIRST_UPDATE = new UserProperty("test1", 2);
+        final UserProperty SECOND_UPDATE = new UserProperty("test2", 1);
+
+        User testUser = new User(USER_ID);
+
+        testUser.addProperty("test_property", FIRST_UPDATE);
+        testUser.addProperty("test_property", SECOND_UPDATE);
+
+        assertTrue(testUser.getProperties().containsKey(PROPERTY_NAME) &&
+                Objects.equals(testUser.getProperties().get(PROPERTY_NAME).getTimestamp(),
+                        FIRST_UPDATE.getTimestamp()) &&
+                testUser.getProperties().get(PROPERTY_NAME).getName().equals(FIRST_UPDATE.getName()));
     }
 
 }
